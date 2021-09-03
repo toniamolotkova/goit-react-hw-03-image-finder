cbf32c0a9c67ca1a5c05d2c282f7ce02f9f6a29b
Antonina Molotkova <molotkovaantonina@gmail.com>
2021-09-03 11:27:58 +0300

    Update Searchbar.js

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -44,8 +44,8 @@ class Searchbar extends Component {
             onChange={this.handleChange}
             className={s.input}
             type="text"
-            autocomplete="off"
-            autofocus
+            autoComplete="off"
+            autoFocus
             placeholder="Search images and photos"
           />
         </form>
